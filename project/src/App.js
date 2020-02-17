222e24cc209e1596e5152089fae2e2465a38ec73
seanaleid <seanaleid@gmail.com>
2020-02-17 12:19:49 -0500

    Initialize repo
      * Initializes repo with starter code, styles, and directories

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from "./components/Title.js";
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
